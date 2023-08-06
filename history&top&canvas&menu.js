d2e5937fcaed8b9062339e5ef7e022483cb60fb4
MSKTeam <126191199+MSKTeam@users.noreply.github.com>
2023-08-07 01:57:45 +0700

    Update and rename history&top&canvas&Menu.js to history&top&canvas&menu.js

--- history&top&canvas&menu.js
+++ history&top&canvas&menu.js
@@ -1,6 +1,5 @@
-// Bắt dầu
-const ImageManager = (function () {
 // Canvas
+const ImageManager = (function () {
   const getModifiedSrc = (imgTag) => {
     const srcRegex = /src=(["'])(.*?)\1/;
     const match = imgTag.match(srcRegex);
@@ -67,6 +66,7 @@ const ImageManager = (function () {
       loadNextImage(imgTags);
     },
   };
+})();
 // Lịch sử
 const limitHistory=100,histories=function(){function i(i,t,e){this.id=i,this.title=t,this.link=e}function t(){localStorage.setItem("History",JSON.stringify(list))}return list=[],null!=localStorage.getItem("History")&&(list=JSON.parse(localStorage.getItem("History"))),(obj={}).addHistory=function(e,s,l){var n=new i(e,s,l);if(null!=list){if(same=list.find(i=>i.id==e),list.length<100){if(same)for(let o in list)list[o].id===e&&list.splice(list.length,0,list.splice(o,1)[0]),t();else list.push(n),t()}else if(same)for(let r in list)list[r].id===e&&list.splice(list.length,0,list.splice(r,1)[0]),t();else list.splice(0,1),list.push(n),t()}else list.push(n),t()},obj}();
 // Fixed
@@ -74,5 +74,3 @@ const nPLElement=document.querySelector(".nPLFix");const initialPosition=nPLElem
 // Chung
 function fungsiSearch(){var a=document.getElementById("Query-input");a.classList.toggle("mystyle")}function myMenu(){var a=document.getElementById("PageList1");a.classList.toggle("shwx")};
 var mybutton=document.getElementById("scTop");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>150||document.documentElement.scrollTop>150){mybutton.style.display="block"}else{mybutton.style.display="none"}}function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};
-// Kết thúc
-})();
